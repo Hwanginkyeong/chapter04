e5e4b2dbbd3917873f3211e40f83f9a966e1be17
hwanginkyeong <inke0831@naver.com>
2021-12-06 11:39:19 +0900

    1st

---
+++ src/com/javaex/ex01/PointApp.java
@@ -0,0 +1,20 @@
+package com.javaex.ex01;
+
+public class PointApp {
+
+	public static void main(String[] args) {
+		
+		Point p01 = new Point(5,7);
+		Point p02 = new Point(10,15);
+		
+		
+		/* <test>
+		 * System.out.println(p01.toString());
+		 */
+		
+		PointList pList = new PintList();
+		
+
+	}
+
+}
